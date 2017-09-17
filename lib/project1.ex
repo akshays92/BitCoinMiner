5fbf34f10da1da45d62bb9dbb2f0d6f922010d4a
Akshay Sharma <akshay.sharma@ufl.edu>
2017-09-17 17:52:58 -0400

    generic variable and ip implementation

--- lib/project1.ex
+++ lib/project1.ex
@@ -6,27 +6,31 @@ defmodule PROJECT1 do
     inputstring = "GoGator"
     input_arguements = (List.to_string(args))
 
-    findIP
-
     if String.contains?(input_arguements,".") do
       IO.puts("This is a slave machine, it should contact the server and only do mining")
-      IO.inspect Node.start(:"two@10.136.70.51") #Should be any random string
-      IO.inspect Node.set_cookie(:"fortune")
-      IO.inspect Node.connect(:"one@10.136.70.51")
-      IO.inspect k = GenServer.call({:CoinServer, :"one@10.136.70.51"}, {:get_k})
+      
+      #get random connection atom to spawn this node
+      IO.inspect Node.start(PROJECT1.ConnectionAtomGenerator.get_connection_atom("192.168.0.13")) 
+      IO.inspect Node.set_cookie(:"DesiBitcoinFarm")
+
+
+      #server connection atom
+      serveratom=String.to_atom("Server"<>"@"<>args)
+      IO.inspect Node.connect(serveratom)
+      IO.inspect k = GenServer.call({:CoinServer,serveratom}, {:get_k})
       IO.inspect string2 = String.duplicate("0",k)
-      IO.inspect sname = {:CoinServer, :"one@10.136.70.51"}
+      IO.inspect sname = {:CoinServer, serveratom}
       spawnXminingThreads(sname, 0, 36, inputstring, string2, 1)
       PROJECT1.OutputPrinter.listen()
     else
       IO.puts("This IS a SERVER machine, it will spawn 3 threads, for mining, generating input string and printing output on the terminal")
-      Node.
-      Node.start(:"one@10.136.70.51")
-      Node.set_cookie(:"fortune")
+      serveratom=String.to_atom("Server"<>"@"<>"192.168.0.16")
+      Node.start(serveratom)
+      Node.set_cookie(:"DesiBitcoinFarm")
       k = String.to_integer(input_arguements)
       string2 = String.duplicate("0",k)
       PROJECT1.Server.start_link(k)
-      sname = {:CoinServer, :"one@10.136.70.51"}
+      sname = {:CoinServer, serveratom}
       spawnXminingThreads(sname, 0, 36, inputstring, string2, 0)
       PROJECT1.OutputPrinter.listen()
     end
