19443ce59bfaa18f8b7d4ca2882b186e7050c1b4
Akshay Sharma <akshay.sharma@ufl.edu>
2017-09-18 17:49:49 -0400

    final commit

--- lib/miner.ex
+++ lib/miner.ex
@@ -1,6 +1,6 @@
 defmodule PROJECT1.Miner do
-    
-    def minecoins(sname, count, inputstring, string2, val) do
+    #Code for implementing hashcash algorithm and mining bitcoins
+    def minecoins(sname, count, inputstring, string2, val) do 
         countStr = Integer.to_string(count,36)
         shaString = String.downcase(:crypto.hash(:sha256, inputstring <> countStr) |> Base.encode16)
         if String.starts_with?(shaString, string2) do
